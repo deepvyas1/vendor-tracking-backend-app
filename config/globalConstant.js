b095a6606462d7130ee5dc5d88f6e53c23d0c384
deepvyas1 <deep.vyas@krayonnz.com>
2021-05-09 20:19:37 +0530

    fix

--- config/globalConstant.js
+++ config/globalConstant.js
@@ -2,11 +2,12 @@
 
 const environment = process.env.NODE_ENV;
 
-let jwtSecretKey, awsAccessKey, awsSecretKey;
+let jwtSecretKey, awsAccessKey, awsSecretKey, cfImageUrl;
 if(environment === "production") {
     jwtSecretKey = process.env.PROD_JWT_KEY;
     awsAccessKey = process.env.PROD_S3_ACCESS_KEY;
     awsSecretKey = process.env.PROD_S3_SECRET_KEY;
+    cfImageUrl = process.env.PROD_IMAGE_CFURL;
 } else if(environment === "development") {
     jwtSecretKey = "vendortracking";
 }
@@ -14,3 +15,4 @@ if(environment === "production") {
 global.jwtSecretKey = jwtSecretKey;
 global.awsAccessKey = awsAccessKey;
 global.awsSecretKey = awsSecretKey;
+global.cfImageUrl = cfImageUrl;
